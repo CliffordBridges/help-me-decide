aed8b7f47caeca7b42f78518abd3c51f9f7eb6da
CliffordBridges <cliffbridges5@gmail.com>
2019-10-11 16:28:09 -0600

    update current working version

--- 04-current-working-script/help_me_decide.py
+++ 04-current-working-script/help_me_decide.py
@@ -1,12 +1,11 @@
-import decisions_functions as decisions
+import os
+import sys
+src_dir = os.path.join(os.getcwd(), '..', '03-src')
+sys.path.append(src_dir)
+import decisions_functions as hmd
 
 
 
-feature_list = decisions.get_feature_list()
-feature_dict = decisions.set_feature_importance(feature_list)
-option_list = decisions.get_option_list()
-option_dict = decisions.rate_each_option(feature_list, option_list)
-option_value_df = pd.DataFrame.from_dict(option_dict).merge(pd.DataFrame(feature_dict).T, left_index=True, right_index=True)
-
-dual_radar_plot(option_value_df, decisions.get_options_for_radar(option_list))
-decisions.print_scores(option_value_df, option_list)
+decision = hmd.Decision()
+decision.build_decision()
+decision.print_results()
